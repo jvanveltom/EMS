96610c69563f8596c1299fffb6bcf75049fc0915
m-vd-linden <m-vd-linden@hotmail.com>
2018-04-18 10:16:17 +0200

    Added absenceUserControl Added DatabaseHandler

---
+++ EMS_System/DatabaseHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace EMS_System
+{
+    static class DatabaseHandler
+    {
+        static private MySqlConnection conn;
+
+        public static void CreateConnection()
+        {
+            conn = new MySqlConnection("server=localhost;user id=root;database=db_ems_bcsi");
+        }
+    }
+}
