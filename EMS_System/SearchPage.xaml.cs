bc1c0bff42d8e8bac61277212b7194b91ce51704
jvanveltom <d214806@edu.rocwb.nl>
2018-04-18 09:25:36 +0200

    Command Class added

--- EMS_System/SearchPage.xaml.cs
+++ EMS_System/SearchPage.xaml.cs
@@ -22,6 +22,19 @@ namespace EMS_System
         public SearchPage()
         {
             InitializeComponent();
+            SetSelectedItemCommand = new RelayCommand(ExecuteStartServicesCommand, CanExecuteStartServicesCommand);
         }
+
+        private bool CanExecuteStartServicesCommand(object obj) // mag ik dit uitvoeren?
+        {
+            return true; // ja / nee
+        }
+
+        private void ExecuteStartServicesCommand(object obj) // wat moet ik uitvoeren
+        {
+            MessageBox.Show("Hoi"); // vul hier de sql mee, in het geval van deze pagina
+        }
+
+        public ICommand SetSelectedItemCommand { get; private set; }
     }
 }
